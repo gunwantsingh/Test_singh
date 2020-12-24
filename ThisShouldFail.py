a3da6d366094b29723f99d9d77d4db9a7180b14c
gunwantsingh <gunwant.s@gmail.com>
2020-12-25 01:05:23 +0530

    Update ThisShouldFail.py

--- ThisShouldFail.py
+++ ThisShouldFail.py
@@ -1,72 +1,193 @@
-package main
+import base64
+import mimetypes
+import os
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.views.decorators.csrf import csrf_exempt
 
-func getMovieQuote() map[string]string {
-	m := make(map[string]string)
-	m["quote"] = "I'll be back."
-	m["movie"] = "The Terminator"
-	m["year"] = "1984"
 
-	return m
+## 01 - Injection Attacks
+
+def norm(s):
+    return s.strip().replace(' ', '').lower()
+
+
+def sql(request):
+    solution_sql = ("SELECT id from Users where first_name = ''; "
+                    "DROP TABLE Users;--';")
+    expected_sql = "'; DROP TABLE Users;--"
+
+    name = request.POST['name'] if request.method == 'POST' else ''
+    correct = (norm(name) == norm(expected_sql))
+
+    return render(request, 'vulnerable/injection/sql.html',
+            {'name': name, 'correct': correct, 'solution_sql': solution_sql})
+
+
+def file_access(request):
+    msg = request.GET.get('msg', '')
+    return render(request, 'vulnerable/injection/file_access.html',
+            {'msg': msg})
+
+
+def user_pic(request):
+    """A view that is vulnerable to malicious file access."""
+
+    base_path = os.path.join(os.path.dirname(__file__), '../../badguys/static/images')
+    filename = request.GET.get('p')
+
+    try:
+        data = open(os.path.join(base_path, filename), 'rb').read()
+    except IOError:
+        if filename.startswith('/'):
+            msg = "That was worth trying, but won't always work."
+        elif filename.startswith('..'):
+            msg = "You're on the right track..."
+        else:
+            msg = "Keep trying..."
+        return render(request, 'vulnerable/injection/file_access.html',
+                {'msg': msg})
+
+    return HttpResponse(data, content_type=mimetypes.guess_type(filename)[0])
+
+
+def code_execution(request):
+    data = ''
+    msg = ''
+    first_name = ''
+    if request.method == 'POST':
+
+        # Clear out a previous success to reset the exercise
+        try:
+            os.unlink('p0wned.txt')
+        except:
+            pass
+
+        first_name = request.POST.get('first_name', '')
+
+        try:
+            # Try it the Python 3 way...
+            exec(base64.decodestring(bytes(first_name, 'ascii')))
+        except TypeError:
+            # Try it the Python 2 way...
+            try:
+                exec(base64.decodestring(first_name))
+            except:
+                pass
+        except:
+            pass
+
+        # Check to see if the attack was successful
+        try:
+            data = open('p0wned.txt').read()
+        except IOError:
+            data = ''
+
+    return render(request, 'vulnerable/injection/code_execution.html',
+            {'first_name': request.POST.get('first_name', ''), 'data': data})
+
+
+## 02 - Broken Authentication & Session Management
+
+
+## 03 - XSS
+
+def xss_form(request):
+    env = {'qs': request.GET.get('qs', 'hello')}
+    response = render(request, 'vulnerable/xss/form.html', env)
+    response.set_cookie(key='monster', value='omnomnomnomnom!')
+    return response
+
+
+def xss_path(request, path='default'):
+    env = {'path': path}
+    return render(request, 'vulnerable/xss/path.html', env)
+
+
+def xss_query(request):
+    env = {'qs': request.GET.get('qs', 'hello')}
+    return render(request, 'vulnerable/xss/query.html', env)
+
+
+## 04 - Insecure Direct Object References
+users = {
+    '1': {
+        'name': 'Foo',
+        'email': 'foo@example.com',
+        'admin': False,
+    },
+    '2': {
+        'name': 'Bar',
+        'email': 'bar@example.com',
+        'admin': True,
+    }
 }
 
-func healthCheck(w http.ResponseWriter, r *http.Request) {
-	// ok: no-direct-write-to-responsewriter
-	w.Write([]byte("alive"))
-}
+def dor_user_profile(request, userid=None):
+    env = {}
+    user_data = users.get(userid)
 
-func indexPage(w http.ResponseWriter, r *http.Request) {
-    const tme = `<html>`
+    if request.method == 'POST':
+        user_data['name'] = request.POST.get('name') or user_data['name']
+        user_data['email'] = request.POST.get('email') or user_data['email']
+        env['updated'] = True
 
-	const template = `
-	<html>
-	<body>
-	  <h1>Random Movie Quotes</h1>
-	  <h2>%s</h2>
-	  <h4>~%s, %s</h4>
-	</body>
-	</html>`
+    env['user_data'] = user_data
+    env['user_id'] = userid
+    return render(request, 'vulnerable/direct_object_references/profile.html', env)
 
-	quote := getMovieQuote()
+## 05 - Security Misconfiguration
 
-	quoteText := quote["quote"]
-	movie := quote["movie"]
-	year := quote["year"]
+def boom(request):
+    raise Exception('boom')
 
-	w.WriteHeader(http.StatusAccepted)
-	// ruleid: no-direct-write-to-responsewriter
-	w.Write([]byte(fmt.Sprintf(template, quoteText, movie, year)))
-}
 
-func errorPage(w http.ResponseWriter, r *http.Request) {
-	params := r.URL.Query()
-	urls, ok := params["url"]
-	if !ok {
-		log.Println("Error")
-		return
-	}
-	url := urls[0]
+## 06 - Sensitive Data Exposure
 
-    const template = `
-	<html>
-	<body>
-	  <h1>error; page not found. <a href="%s">go back</a></h1>
-	</body>
-	</html>`
+def exposure_login(request):
+    return redirect('exposure')
 
-	w.WriteHeader(http.StatusAccepted)
-	// ruleid: no-direct-write-to-responsewriter
-	w.Write([]byte(fmt.Sprintf(template, url)))
-}
 
-func main() {
-	http.HandleFunc("/", indexPage)
-	http.HandleFunc("/error", errorPage)
-	http.ListenAndServe(":8080", nil)
-}
+## 07 - Missing Function Level Access Control
+
+def missing_access_control(request):
+    env = {}
+    if request.GET.get('action') == 'admin':
+        return render(request, 'vulnerable/access_control/admin.html', env)
+    return render(request, 'vulnerable/access_control/non_admin.html', env)
+
+
+## 08 - CSRF
+
+@csrf_exempt
+def csrf_image(request):
+    env = {'qs': request.GET.get('qs', '')}
+    return render(request, 'vulnerable/csrf/image.html', env)
+
+
+## 09 - Using Known Vulnerable Components
+# No exercise, just discussion?
+
+
+## 10 - Unvalidated Redirects & Forwards
+
+def unvalidated_redirect(request):
+    url = request.GET.get('url')
+    return redirect(url)
+
+
+def unvalidated_forward(request):
+    forward = request.GET.get('fwd')
+    function = globals().get(forward)
+
+    if function:
+        return function(request)
+
+    env = {'fwd': forward}
+    return render(request, 'vulnerable/redirects/forward_failed.html', env)
+
+def admin(request):
+    return render(request, 'vulnerable/redirects/admin.html', {})
 
